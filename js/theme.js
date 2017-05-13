47f977877eb1ef38e4048ced8aba2864327630de
rhossain <hossain.robin007@gmail.com>
2017-05-13 17:18:12 +0600

    Slider update

--- js/theme.js
+++ js/theme.js
@@ -24,19 +24,52 @@
         var owl = $('.owl-carousel');
         owl.owlCarousel({
             navigation : true,
-            slideSpeed : 300,
-            paginationSpeed : 400,
+            // nav:true,
+            autoplay: false,
+    		autoplayTimeout: 5000,
+    		loop: true,
+            slideSpeed : 500,
+            paginationSpeed : 500,
             items : 1, 
             itemsDesktop : false,
             itemsDesktopSmall : false,
             itemsTablet: false,
             itemsMobile : false
         });
-        owl.on('changed.owl.carousel', function(event) {
-            var item = event.item.index - 2;     // Position of the current item
-            $('h1').removeClass('animated bounce');
-            $('.owl-item').not('.cloned').eq(item).find('h1').addClass('animated bounce');
-        });
+        // Custom Navigation Events
+		$('.owl-next').click(function(){
+		    owl.trigger('next.owl.carousel');
+		});
+		$('.owl-prev').click(function(){
+		    owl.trigger('prev.owl.carousel');
+		});
+        // add animate.css class(es) to the elements to be animated
+		function setAnimation ( _elem, _InOut ) {
+			// Store all animationend event name in a string.
+			// cf animate.css documentation
+			var animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+			_elem.each ( function () {
+				var $elem = $(this);
+				var $animationType = 'animated ' + $elem.data( 'animation-' + _InOut );
+
+				$elem.addClass($animationType).one(animationEndEvent, function () {
+					$elem.removeClass($animationType); // remove animate.css Class at the end of the animations
+				});
+			});
+		}
+		// Fired before current slide change
+		owl.on('change.owl.carousel', function(event) {
+			var $currentItem = $('.owl-item', owl).eq(event.item.index);
+			var $elemsToanim = $currentItem.find("[data-animation-out]");
+			setAnimation ($elemsToanim, 'out');
+		});
+		// Fired after current slide has been changed
+		owl.on('changed.owl.carousel', function(event) {
+			var $currentItem = $('.owl-item', owl).eq(event.item.index);
+			var $elemsToanim = $currentItem.find("[data-animation-in]");
+			setAnimation ($elemsToanim, 'in');
+		});
 
         checkWindowSize();
         $(window).resize(checkWindowSize);
