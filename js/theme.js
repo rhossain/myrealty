495679f68447e30ad1f67a63f861389e37d57872
rhossain <hossain.robin007@gmail.com>
2017-08-27 23:30:24 +0600

    Add agent section for homepage

--- js/theme.js
+++ js/theme.js
@@ -132,7 +132,32 @@
 		            loop:false
 		        }
 		    }
-		});
+        });
+        
+        $('#agent-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1,
+                    nav:true
+                },
+                580:{
+                    items:2,
+                    nav:false
+                },
+                767:{
+                    items:3,
+                    nav:false
+                },
+                1000:{
+                    items:4,
+                    nav:true,
+                    loop:false
+                }
+            }
+        });
 
         // Switching between Login & Regiser form
         function showRegisterForm(){
